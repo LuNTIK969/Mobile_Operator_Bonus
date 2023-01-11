9dd3e4d024aa759ebe225cc88eba6907b400d7a7
LuNTIK969 <nasa.969@gmail.com>
2023-01-11 14:26:53 +0300

    code upd

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,15 @@ public class Main {
         int bonus;
 
         if (up > 1_001) {
-            bonus = up *1 / 100;
-        }
-        else {
+            bonus = up * 1 / 100;
+        } else {
             bonus = 0;
         }
 
-        int F_up = up + bonus;
-        int F_balance = balance + F_up;
+        int finalUp = up + bonus;
+        int finalBalance = balance + finalUp;
 
         System.out.println("Баланс пополнен на " + up + "р. \nТак как у нас проходит акция \"Пополни баланс от 1000р. единовременно и получи подарок!\"," +
-                " сумма вашего пополения увеличена на " + bonus + "р. \nИтоговое пополнение баланса " + F_up + "р. \nТекущий баланс: " + F_balance + "р.");
+                " сумма вашего пополения увеличена на " + bonus + "р. \nИтоговое пополнение баланса " + finalUp + "р. \nТекущий баланс: " + finalBalance + "р.");
     }
 }
